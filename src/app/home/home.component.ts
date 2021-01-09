9a727bcc14c98568b3ee42581289fc582a2f09ae
RoloufGoudebin <goudeloufrobin@gmail.com>
2021-01-09 22:08:41 +0100

    add meta facebook

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Title } from '@angular/platform-browser';
+import { Title, Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-home',
@@ -8,10 +8,19 @@ import { Title } from '@angular/platform-browser';
 })
 export class HomeComponent implements OnInit {
 
-  constructor( private titleService:Title) { }
+  constructor( private titleService:Title, private meta:Meta) { }
 
   ngOnInit(): void {
+
     this.titleService.setTitle("Mamoot, l'agence digitale qui en impose");
+
+    this.meta.addTags([
+      { name: 'go:site_name', content: 'Mamoot' },
+      { name: 'go:locale', content: 'fr_be' },
+      { name: 'go:type', content: 'sebsite' },
+      { name: 'go:description', content: 'Créations de solutions digitales, stratégie de communication et bien plus encore !' },
+      { name: 'go:image', content: 'https://www.mamoot.be/assets/img/Mamoot.png' }
+    ], true);
   }
 
 
