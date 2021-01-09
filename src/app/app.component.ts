a97205a3f86bb443284950352c84e4e7e1838048
RoloufGoudebin <goudeloufrobin@gmail.com>
2021-01-09 22:20:59 +0100

    Facebook3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Title, Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'mamootv3';
+export class AppComponent implements OnInit {
+
+  constructor( private titleService:Title, private meta:Meta) { }
+
+  ngOnInit(): void {
+
+    this.titleService.setTitle("Mamoot, l'agence digitale qui en impose");
+
+    this.meta.addTags([
+      { name: 'go:site_name', content: 'Mamoot' },
+      { name: 'go:locale', content: 'fr_be' },
+      { name: 'go:type', content: 'sebsite' },
+      { name: 'go:description', content: 'Créations de solutions digitales, stratégie de communication et bien plus encore !' },
+      { name: 'go:image', content: 'https://www.mamoot.be/assets/img/Mamoot.png' }
+    ], true);
+  }
+
+
+  scroll(el: HTMLElement) {
+    el.scrollIntoView({ behavior: 'smooth' });
+  }
+
+
 }
